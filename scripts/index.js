3ad831d04d724c1129ad70cf9d57164b32edacb1
Rajan Mali <mysteriouskiller50@gmail.com>
2021-07-05 13:33:16 +0545

    search bar functionality for google querying

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector('.menu');
 const openBtn = document.querySelector('.ui__btn');
 const searchBtn = document.querySelector('.search__icon');
+const searchInput = document.querySelector('.search__input');
 
 openBtn.addEventListener('click', () => {
   menuScale();
@@ -9,6 +10,11 @@ openBtn.addEventListener('click', () => {
 
 searchBtn.addEventListener('click', () => {
   !!menu.classList.contains('active') && menuScale();
+});
+
+searchInput.addEventListener('keypress', (event) => {
+  event.key === 'Enter' &&
+    window.open(`http://google.com/search?q=${searchInput.value}`);
 });
 
 const menuScale = () => {
